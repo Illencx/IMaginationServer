1c21f1f155781d434beaf8d86862104eef56b871
Leo <Illencx@outlook.com>
2021-09-05 22:46:30 +0800

    add gitignore and redis data access

---
+++ redis.js
@@ -0,0 +1,16 @@
+const redis = require('redis');
+const client = redis.createClient(6379,'47.111.121.61')
+client.on('connect', function () {
+    console.log('success');
+})
+
+client.set('admin', 'leo',function(){
+    console.log('set complete');
+})
+
+ client.get('admin',function(err,res){
+    console.log('res',res);
+})
+client.PING(function(err,res){
+    console.log('res',res);
+})
